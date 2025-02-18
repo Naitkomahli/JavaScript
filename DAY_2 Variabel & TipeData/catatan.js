196bb0d5826b89d264d89370bdd5684914c2d362
Naitkomahli <ilhanoktian31@gmail.com>
2025-02-18 12:23:57 +0700

    JS d2

---
+++ DAY_2 Variabel & TipeData/catatan.js
@@ -0,0 +1,20 @@
+var nama = "Budi"; // Bisa  diubah, tapi tidak dipakai lagi
+let umur = 21; // Bisa diubah, lebih modern
+const PI = 3.14; // Tidak bisa diubah
+
+//Contoh
+let nama = "John"; // String
+let umur2 = 25; // Number
+let isStudent = true; // Boolean
+let alamat = null; // Null
+let pekerjaan; // Undefined
+let id = Symbol("id"); // Symbol
+let bigNumber = 1234567890123456789012345678901234567890n; // BigInt
+
+let person = { nama: "John", umur: 25 }; // Object
+let numbers = [1, 2, 3]; // Array
+function greet() {
+  console.log("Hello!");
+} // Function
+let today = new Date(); // Date
+let regex = /ab+c/; // RegExp
---
+++ DAY_2 Variabel & TipeData/script.js
@@ -0,0 +1,6 @@
+let nama = prompt("Masukkan nama kamu:");
+let umur = prompt("Masukkan umur kamu:");
+
+console.log("Nama:", nama);
+console.log("Umur:", umur);
+alert("Halo, " + nama + "! Umur kamu " + umur + " tahun.");

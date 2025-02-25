242248d7bd7f11b2672935dd6a82afda37a25a54
Naitkomahli <ilhanoktian31@gmail.com>
2025-02-25 20:59:55 +0700

    JS d5

---
+++ DAY_5_perulangan/Belajar/script.js
@@ -0,0 +1,54 @@
+console.log("Perulangan for");
+
+for (inisialisasi; kondisi; iterasi) {
+    // kode yang akan dijalankan
+  }
+
+for (let i = 0; i < 5; i++) {
+    console.log("Iterasi ke " + i);
+    }
+
+// Output:
+// Perulangan for
+// Iterasi ke 0
+// Iterasi ke 1
+// Iterasi ke 2
+// Iterasi ke 3
+// Iterasi ke 4
+
+console.log("Perulangan while");
+
+while (kondisi) {
+    // Kode yang diulang
+}
+
+let i = 0;
+while (i < 5) {
+    console.log("Iterasi ke-" + i);
+    i++;
+}
+
+// Iterasi ke-0
+// Iterasi ke-1
+// Iterasi ke-2
+// Iterasi ke-3
+// Iterasi ke-4
+
+console.log("Perulangan do while");
+
+do {
+    // Kode yang diulang
+} while (kondisi);
+
+let x = 0;
+
+do {
+    console.log("Iterasi ke-" + i);
+    x++;
+} while (i < 5);
+
+// Iterasi ke-0
+// Iterasi ke-1
+// Iterasi ke-2
+// Iterasi ke-3
+// Iterasi ke-4
---
+++ DAY_5_perulangan/Latihan/script.js
@@ -0,0 +1,18 @@
+console.log("For loop");
+for (let i = 1; i <= 10; i++){
+    console.log("Iterasi ke " + i);
+}
+
+console.log("While loop");
+let j = 10;
+while (j>=1){
+    console.log("Iterasi ke " + j);
+    j--;
+}
+
+console.log("do-while loop"); 
+let k = 5;
+do {
+    console.log("Angka: " + k);
+    k--;
+} while (k >= 1);
